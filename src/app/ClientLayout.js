7d20e0da43014352b159bfb6b1638628233a101d
Yash Kumar <kyash03@student.ubc.ca>
2025-03-29 23:25:16 -0700

    remove scroll to top effect

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -1,14 +1,9 @@
 "use client";
-import { useEffect } from "react";
 import ScrollToTopButton from "@/components/ui/ScrollToTopButton";
 import Footer from "@/components/layout/Footer";
 import { AppProvider } from "@/context/AppContext";
 
 export default function ClientLayout({ children }) {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <AppProvider>
       <main style={{ color: "var(--text-primary)" }}>
